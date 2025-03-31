682559fcaa968a9ee74d76081a85b259dda51a27
Duran Gezer <durangezer2004@gmail.com>
2025-04-01 02:01:43 +0300

    b

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -28,6 +28,16 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/about.js"
+    ],
+    "/contact": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/contact.js"
+    ],
+    "/projects": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/projects.js"
     ]
   },
   "ampFirstPages": []
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1792366a292fda12"; }
+/******/ 		__webpack_require__.h = function() { return "c2f87f634cdf8234"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
